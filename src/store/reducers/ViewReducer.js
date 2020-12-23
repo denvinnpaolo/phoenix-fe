10040ee089ab7568d7bdd4322546252320028c47
Denvinn Magsino <denvinnpaolo@gmail.com>
2020-12-22 23:45:03 -0800

    finished reducer

--- src/store/reducers/ViewReducer.js
+++ src/store/reducers/ViewReducer.js
@@ -5,7 +5,33 @@ export {
 } from '../actions/index.js'
 
 const initialState = {
-    postedById = [],
+    postedById : [],
     error: null,
     isFetching: false
 }
+
+function ViewReducer(state = initialState, action){
+    switch(action.type){
+        case FETCH_VIEWBYID_LOADING:
+            return {
+                ...state,
+                isFetching: true
+            }
+        case FETCH_VIEWBYID_SUCCESS:
+            return{
+                ...state,
+                postedById: action.payload,
+                isFetching: false
+            }
+        case FETCH_VIEWBYID_FAILURE:
+            return{
+                ...state,
+                error: action.payload,
+                isFetching: false
+            }
+        default:
+            return state
+    }
+}
+
+export default ViewReducer;
