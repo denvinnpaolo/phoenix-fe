41b6d5824666ff593969d67b793a4941d21542ca
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-01-24 21:58:01 -0800

    fixed texts alignment

--- src/components/landing/website/contact/Contact.js
+++ src/components/landing/website/contact/Contact.js
@@ -6,7 +6,7 @@ const Contact = () => {
         <div id="main-contact-container">
             <Nav />
                         <div id="contact-header" style={{fontSize:"3.4em", fontWeight: "bold"}}>Contact</div>
-                        <div><span>We would love to hear from you so please feel free to send a message.</span></div>
+                        <div id="contacts-texts"><span>We would love to hear from you so please feel free to send a message.</span></div>
 
         </div>
     )
