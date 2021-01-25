b9d5624d93cfe05b8c1076cf50b7d8104e6066ca
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-01-24 22:25:36 -0800

    added forms and div

--- src/components/landing/website/contact/Contact.js
+++ src/components/landing/website/contact/Contact.js
@@ -1,13 +1,30 @@
 import React from 'react';
 import Nav from '../nav/Nav.js';
+import Footer from '../footer/Footer.js'
 
 const Contact = () => {
     return(
         <div id="main-contact-container">
             <Nav />
-                        <div id="contact-header" style={{fontSize:"3.4em", fontWeight: "bold"}}>Contact</div>
-                        <div id="contacts-texts"><span>We would love to hear from you so please feel free to send a message.</span></div>
-
+            <div id="contact-header" style={{fontSize:"3.4em", fontWeight: "bold"}}>Contact</div>
+            <div id="contacts-texts">
+                <span>We would love to hear from you so please feel free to send a message.</span>
+            </div>
+            <div id="contacts-form-container">
+                <div id='contacts-forms'>
+                    <div>
+                    <span>NAME</span>
+                    <input />
+                    </div>
+                    
+                    <span>EMAIL</span>
+                    <input />
+                    <span>MESSAGE</span>
+                    <input />
+                    <button> Submit</button>
+                </div>
+            </div>
+            <Footer />
         </div>
     )
 };
