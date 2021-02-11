2128055855a85944beda09aa73e34b4df7389b04
Denvinn Magsino <denvinnpaolo@gmail.com>
2021-02-10 23:59:18 -0800

    added marker

--- src/components/dashboard/map/Map.js
+++ src/components/dashboard/map/Map.js
@@ -65,6 +65,9 @@ const MapComponent = () => {
             <Marker
                 position={{ lat: 41.756795, lng: -73.954298 }}
             />
+            <Marker
+                position={{ lat: 41.756795, lng: -73.954298 }}
+            />
         </GoogleMap>
      ));
 
